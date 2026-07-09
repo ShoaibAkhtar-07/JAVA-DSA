1a11b921b0256dc0868f5cf7b9c13132edecebd2
ShoaibAkhtar-07 <shoaibakhtar987987@gmail.com>
2026-07-09 15:50:39 +0530

    Reversing the Stack

--- Stack/stackB.java
+++ Stack/stackB.java
@@ -4,6 +4,13 @@ import java.util.*;
 
 public class stackB {
 
+    // Printing a Stack
+    public static void printstack(Stack<Integer> s) {
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
+    }
+
     // Pushing at the Bottom of the Stack
     public static void pushAtBottom(Stack<Integer> s, int data) {
         if (s.isEmpty()) {
@@ -16,9 +23,8 @@ public class stackB {
 
     }
 
-    
-    // Reversing the Stack
-    public static String reverseStack(String str) {
+    // Reversing the String Using Stack
+    public static String reverseString(String str) {
         Stack<Character> s = new Stack<>();
         int i = 0;
         while (i < str.length()) {
@@ -30,21 +36,28 @@ public class stackB {
         while (!s.empty()) {
             char curr = s.pop();
             st.append(curr);
-            // System.out.print(s.pop());
         }
         return st.toString();
+    }
 
+    // Reversing the Stack
+    public static void reverseStack(Stack<Integer> s) {
+        if (s.isEmpty()) {
+            return;
+        }
+
+        int top = s.pop();
+        reverseStack(s);
+        pushAtBottom(s, top);
     }
 
     public static void main(String[] args) {
-        // Stack<Integer> s = new Stack<>();
-        // s.push(1);
-        // s.push(2);
-        // s.push(3);
-        // pushAtBottom(s, 4);
-        // while (!s.isEmpty()) {
-        // System.out.println(s.pop());
-        // }
-        System.out.println(reverseStack("shoaib"));
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        reverseStack(s);
+        printstack(s);
+        // System.out.println(reverseString("shoaib"));
     }
 }
