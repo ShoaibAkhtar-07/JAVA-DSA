6e5c0280143501041baced381abd387fa54aad02
ShoaibAkhtar-07 <shoaibakhtar987987@gmail.com>
2026-07-18 16:14:46 +0530

    Checking wheather the String is contains Valid Parenthesis or NOt

---
+++ Stack/ValidParenthesis.java
@@ -0,0 +1,37 @@
+package Stack;
+
+import java.util.Stack;
+
+public class ValidParenthesis {
+
+    public static boolean isValid(String str) {
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '(' || ch == '[' || ch == '{') {
+                st.push(ch);
+            } else {
+                if (st.isEmpty()) {
+                    return false;
+                }
+                if ((st.peek() == '(' && ch == ')')
+                        || (st.peek() == '[' && ch == ']')
+                        || (st.peek() == '{' && ch == '}')) {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        if (st.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "({})[]()";
+        System.out.println(isValid(str));
+    }
+}
