1e240f3bad0c88f4baa69148a5db07e0e141f15a
ShoaibAkhtar-07 <shoaibakhtar987987@gmail.com>
2026-07-25 22:57:21 +0530

    Fractional Knapsack solution usig Greedy Approach for Unsorted values

--- GreedyAlgo/FractionalKnapsack.java
+++ GreedyAlgo/FractionalKnapsack.java
@@ -1,23 +1,30 @@
 package GreedyAlgo;
 
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class FractionalKnapsack {
     public static void main(String[] args) {
-        int[] value = { 500 };
-        int[] weight = { 30 };
-        int capacity = 10;
+        int[] value = { 120, 100, 60 };
+        int[] weight = { 30, 20, 10 };
+        int capacity = 50;
 
-        double[] ratio = new double[value.length];
+        double[][] ratio = new double[value.length][2];
         for (int i = 0; i < value.length; i++) {
-            ratio[i] = value[i] / (double) weight[i];
+            ratio[i][0] = i;
+            ratio[i][1] = value[i] / (double) weight[i];
         }
+        Arrays.sort(ratio, Comparator.comparingDouble(o -> o[1]));
 
         double ans = 0;
-        for (int i = 0; i < ratio.length; i++) {
-            if (capacity >= weight[i]) {
-                ans += value[i];
-                capacity -= weight[i];
+        for (int i = ratio.length - 1; i >= 0; i--) {
+            int idx = (int) ratio[i][0];
+            if (capacity >= weight[idx]) {
+                ans += value[idx];
+                capacity -= weight[idx];
             } else {
-                ans += (double) ratio[i] * capacity;
+                ans += (double) ratio[i][1] * capacity;
                 capacity = 0;
                 break;
             }
